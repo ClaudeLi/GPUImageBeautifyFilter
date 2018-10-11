282a2975e71a7ff657bebdc1cbbf93a701027314
claudeli <claudeli@yeah.net>
2018-10-11 10:55:50 +0800

    1.0

---
+++ GPUImageBeautifyFilter/Classes/GPUImageBeautyFilter.h
@@ -0,0 +1,25 @@
+#if __has_include(<GPUImage/GPUImage.h>)
+#import <GPUImage/GPUImage.h>
+#elif __has_include("GPUImage/GPUImage.h")
+#import "GPUImage/GPUImage.h"
+#else
+#import "GPUImage.h"
+#endif
+@interface GPUImageBeautyFilter : GPUImageFilter 
+
+/**
+ 美颜程度
+ */
+@property (nonatomic, assign) CGFloat beautyLevel;
+
+/**
+ 美白程度
+ */
+@property (nonatomic, assign) CGFloat brightLevel;
+
+/**
+ 色调强度
+ */
+@property (nonatomic, assign) CGFloat toneLevel;
+
+@end
